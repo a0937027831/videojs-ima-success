b3499e63d9c5405f6e0a1dc42a2e1684d1c53fde
jkschou <a0937027831@gmail.com>
2023-03-22 03:28:46 +0000

    fix http to https

--- app.vue
+++ app.vue
@@ -18,7 +18,7 @@ import "videojs-ima/src/css/videojs.ima.css";
 useHead({
   script: [
     {
-      src: "http://imasdk.googleapis.com/js/sdkloader/ima3.js",
+      src: "https://imasdk.googleapis.com/js/sdkloader/ima3.js",
     },
   ],
 });
@@ -60,12 +60,12 @@ function onReady() {
     debug: true,
     id: "video",
     adTagUrl:
-      "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator=",
+      "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator=",
   };
   player.ima(adsOptions);
   player.src({
-    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-    type: 'video/mp4',
+    src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    type: "video/mp4",
   });
 }
 
@@ -74,6 +74,4 @@ onBeforeUnmount(() => {
     player.dispose();
   }
 });
-
-
 </script>
